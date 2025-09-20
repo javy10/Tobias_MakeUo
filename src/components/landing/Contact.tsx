112bf1c574764592c2528b4903dcf6ef99c22182
Javier Beltran <JaviierB6@gmail.com>
2025-09-20 02:45:48 +0000

    con respecto al mapa deja por defecto este tipo de vista, no se como se

--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -79,7 +79,7 @@ export function Contact() {
         </Card>
         <div className="rounded-xl overflow-hidden shadow-xl aspect-video md:aspect-auto md:h-full">
            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d968.992642197607!2d-88.95777717325936!3d14.09255622080352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTTCsDA1JzMzLjEiTiA4OMKwNTcnMjUuOSJX!5e0!3m2!1ses-419!2ssv!4v1723067644670!5m2!1ses-419!2ssv"
+              src="https://maps.google.com/maps?q=14.092523,-88.957206&t=k&z=19&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
